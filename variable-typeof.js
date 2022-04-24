41f537eb1e32c558270849322a24b65572074489
Pavel <97633675+Rodon1234@users.noreply.github.com>
2022-04-24 22:34:00 +0300

    Add files via upload

---
+++ variable-typeof.js
@@ -0,0 +1,24 @@
+// let nimfa
+// nimfa = 15
+// console.log(nimfa)
+
+
+// let nimfa1 = 15
+// let nimfa2 = 20
+// let result = nimfa1 + nimfa2
+// console.log(result)
+
+let n1 = 10,
+    n2 = 30,
+    n3 = "Hello",    
+    n4 = 20;
+
+let let_type1 = typeof n1;   
+let let_type2 = typeof n2;
+let let_type3 = typeof n3;
+let let_type4 = typeof n4;
+
+console.log(let_type1)
+console.log(let_type2)
+console.log(let_type3)
+console.log(let_type4)
