fe86039e629558268251d1438109e1359423ae59
Pavel <97633675+Rodon1234@users.noreply.github.com>
2022-04-26 12:30:14 +0300

    Add files via upload

---
+++ concatenation(string).js
@@ -0,0 +1,15 @@
+let Nika1 = 20
+let Nika2 = "HellO!"
+
+let result = Nika1 + Nika2
+
+let n1 = 10,
+    n2 = 30,
+    Name_3 = "200",
+    n4 = 300;
+
+let sum_body_string = String(n2 + n4);
+
+let let_type1 = typeof(sum_body_string);
+console.log (sum_body_string)
+console.log (let_type1)
