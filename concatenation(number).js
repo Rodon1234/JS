4d04f59785a8c6ab98660c5a833b4f234c10c526
Pavel <97633675+Rodon1234@users.noreply.github.com>
2022-05-03 21:00:15 +0300

    Update concatenation(number).js

--- concatenation(number).js
+++ concatenation(number).js
@@ -7,3 +7,10 @@ let n1 = 10,
     n2 = 30,
     Name_3 = "200",
     n4 = 300;
+
+let sum_body = +Name_3 + n4;
+
+let let_type1 = typeof(sum_body);
+console.log (sum_body)
+console.log (let_type1)
+
